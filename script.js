87c60fe61d7be27445f907d7773d30890c90b548
Jonathan Werz <jdwerz83@gmail.com>
2021-12-05 16:48:13 -0500

    omfg the indent almost ruined me

--- script.js
+++ script.js
@@ -12,51 +12,51 @@ function generatePassword(){
   var allTheThings = '';
   var password = '';
 
-var passLength = prompt('Password length between 8 and 128 characters?')
+  var passLength = prompt('Password length between 8 and 128 characters?')
 
-if (passLength >= 8 && passLength <= 128) {
-  console.log(passLength)
-} else {
-  alert('Please pick a number 8 or greater, but no more than 128')
-  return '';
-}
+  if (passLength >= 8 && passLength <= 128) {
+    console.log(passLength)
+  } else {
+    alert('Please pick a number 8 or greater, but no more than 128')
+    return '';
+  }
 
-var watLowerCase = confirm('Use lower case characters?')
-var watUpperCase = confirm('Use upper case characters?')
-var watNumerals = confirm('Use numerals?')
-var watSpecial = confirm('Use special characters?')
+  var watLowerCase = confirm('Use lower case characters?')
+  var watUpperCase = confirm('Use upper case characters?')
+  var watNumerals = confirm('Use numerals?')
+  var watSpecial = confirm('Use special characters?')
 
-if (watLowerCase) {
-  allTheThings += lowerCase
-}
-console.log(watLowerCase)
+  if (watLowerCase) {
+    allTheThings += lowerCase
+  }
+  console.log(watLowerCase)
 
-if (watUpperCase) {
-  allTheThings += upperCase
-}
-console.log(watUpperCase)
+  if (watUpperCase) {
+    allTheThings += upperCase
+  }
+  console.log(watUpperCase)
 
-if (watNumerals) {
-  allTheThings += numerals
-}
-console.log(watNumerals)
+  if (watNumerals) {
+    allTheThings += numerals
+  }
+  console.log(watNumerals)
 
-if (watSpecial) {
-  allTheThings += special
-}
-console.log(watSpecial)
+  if (watSpecial) {
+    allTheThings += special
+  }
+  console.log(watSpecial)
 
-if (
-  !watLowerCase && !watUpperCase && !watNumerals && !watSpecial
-) {
-  return alert('Help me to help you...');  
-}
+  if (
+    !watLowerCase && !watUpperCase && !watNumerals && !watSpecial
+  ) {
+    return alert('Help me to help you...');  
+  }
 
-for (let i = 0; i < passLength; i++) {
-  password += allTheThings[Math.floor(Math.random () * allTheThings.length)];
-}
+  for (let i = 0; i < passLength; i++) {
+    password += allTheThings[Math.floor(Math.random () * allTheThings.length)];
+  }
 
-return password;
+  return password;
 }
 
 // Write password to the #password input
